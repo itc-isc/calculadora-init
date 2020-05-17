45aafefc21f622b70c5dc8b3e251b9c135fc5332
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2020-05-17 16:28:57 -0600

    Agregando Calculadora.suma

---
+++ src/main/java/wsCalculadora/iscrquinter/hotmail/com/Calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package wsCalculadora.iscrquinter.hotmail.com.Calculadora;
+
+public class Calculadora {
+
+	public double suma(double x, double y) {
+		return x + y;
+	}
+    
+}
--- src/test/java/wsCalculadora/iscrquinter/hotmail/com/AppTest.java
+++ src/test/java/wsCalculadora/iscrquinter/hotmail/com/AppTest.java
@@ -4,11 +4,20 @@
 package wsCalculadora.iscrquinter.hotmail.com;
 
 import org.junit.Test;
+
+import wsCalculadora.iscrquinter.hotmail.com.Calculadora.Calculadora;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test public void testSuma() {
+        Calculadora c = new Calculadora();
+
+        assertTrue(c.suma(2,3)==5);
     }
 }
